0cf98de37d5f641352dde7954de37a460fe8cf7f
CarlosDev <cazc.zarate94@gmail.com>
2023-03-23 00:16:58 -0600

    terminado con 2 versiones una en clase y una investigando

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,13 +1,71 @@
+/////////////////////////////////////////////////////////////////VERSION1///////////////////////////////////////////
+/*
 // API
 const API_ENDPOINT = 'https://yesno.wtf/api';
 
-/**
- * STEPS:
- *
- * 1. Create a fetchAnswer function and call the API
- * 2. Output the API's response
- * 3. Attach fetchAnswer to an event listener
- * 4. Clear output after 3 seconds
- * 5. Optional: add loading/error states
- *
- */
+// DOM elements
+const input = document.getElementById('input');
+const button = document.getElementById('button');
+const answer = document.getElementById('answer');
+const error = document.getElementById('error');
+
+//get question from input
+const question = input.value;
+
+// Send GET request to API endpoint
+async function fetchAnswer() {
+  let response = await fetch(`${API_ENDPOINT}?q=${question}`);
+  let data = await response.json();
+
+    // Output API's response
+  answer.innerHTML = data.answer;
+  }
+// Add event listener to button
+button.addEventListener('click', fetchAnswer);
+*/
+
+////////////////////////////////////////////////////////////Version 2.0/////////////////////////////////////////////
+  // API
+  const API_ENDPOINT = 'https://yesno.wtf/api';
+
+  // DOM elements
+  const input = document.getElementById('input');
+  const button = document.getElementById('button');
+  const answer = document.getElementById('answer');
+  const error = document.getElementById('error');
+  
+  // Add event listener to button
+  button.addEventListener('click', fetchAnswer);
+  
+  
+  // Fetch answer from API
+  function fetchAnswer() {
+    // Clear previous output
+    answer.innerHTML = '';
+    error.innerHTML = '';
+  
+    // Get question from input
+    const question = input.value;
+    
+    // Send GET request to API endpoint
+    fetch(`${API_ENDPOINT}?q=${question}`)
+      .then(response => response.json())
+      .then(data => {
+        // Output API's response
+        answer.innerHTML = data.answer;
+      })
+      .catch(error => {
+        // Output error message
+        error.innerHTML = 'Error';
+      });
+  
+    // Clear output after 3 seconds
+    setTimeout(() => {
+      input.value = '';
+      answer.innerHTML = '';
+      error.innerHTML = '';
+    }, 3000);
+  }
+  
+  
+  
