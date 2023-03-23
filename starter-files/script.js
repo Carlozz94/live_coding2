e0f20500e5645adf64ee24e8d6ca8c0d087dc507
CarlosDev <cazc.zarate94@gmail.com>
2023-03-23 00:58:03 -0600

    se agregan comentarios del compañero

--- starter-files/script.js
+++ starter-files/script.js
@@ -24,6 +24,13 @@ async function fetchAnswer() {
 button.addEventListener('click', fetchAnswer);
 */
 
+
+/*
+Comentarios
+Estudiar mas la sintaxis de Js ya que no recuerda la mayoria de terminos
+Mas enfoque al detalle en algunos metodos que faltan
+y mas ejercicios para que queden claro los temas de fetch, asycn, await y eventos de botones
+*/
 ////////////////////////////////////////////////////////////Version 2.0/////////////////////////////////////////////
   // API
   const API_ENDPOINT = 'https://yesno.wtf/api';
